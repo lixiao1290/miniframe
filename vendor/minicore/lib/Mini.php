596fe752109cf0ae4265df7fec9f3b1c9543ded7
lixiao <lixiao.god@163.com>
2018-02-02 11:27:56 +0800

    依赖注入

--- vendor/minicore/lib/Mini.php
+++ vendor/minicore/lib/Mini.php
@@ -31,45 +31,66 @@ class Mini
     }
 
     /**
-     *
+     *给出类名，获得对象
      */
     public static function createObj($name)
     {
         $reflectionClass = new \ReflectionClass($name);
         $reflectionMethod = $reflectionClass->getConstructor();
-        if ($reflectionClass->getConstructor() && $paremeters = $reflectionClass->getConstructor()->getParameters()) {
-            $actualParameters = array();
-            foreach ($paremeters as $parameter) {
-
-                $className = $parameter->getClass()->getName();
-                if (class_exists($className)) {
-                    $actualParameters[$parameter->getName()] = static::creatObj($className);
+        if ($reflectionClass->getConstructor()) {
+            $paremeters = $reflectionClass->getConstructor()->getParameters();
+            if ($paremeters) {
+                $actualParameters = array();
+                foreach ($paremeters as $parameter) {
+                    $className = $parameter->getClass()->getName();
+                    try {
+                        if (class_exists($className)) {
+                            $actualParameters[] = static::creatObj($className);
+                        } else {
+                            throw new \Exception("class " . $classname . "  not found");
+                        }
+                    } catch (\Exception $e) {
+                        echo $e->getMessage();
+                    }
                 }
-            }
 //            var_dump($name, $actualParameters);
-            $obj = $reflectionClass->newInstanceArgs($actualParameters);
+                $obj = $reflectionClass->newInstanceArgs($actualParameters);
+            } else {
+                $obj = $reflectionClass->newInstance();
+            }
         } else {
-            $obj = $reflectionClass->newInstance();
+            $obj = $reflectionClass->newInstanceWithoutConstructor();
         }
         return $obj;
     }
 
+    /**
+     * 给粗参数类名，返回对象
+     * @param $classname
+     * @param $args
+     * @return object
+     */
     public static function creatObjByArgs($classname, $args)
     {
         try {
             if (class_exists($classname)) {
                 $reflectionClass = new \ReflectionClass($classname);
                 $reflectionMethod = $reflectionClass->getConstructor();
-                if ($reflectionClass->getConstructor() && $paremeters = $reflectionClass->getConstructor()->getParameters()) {
-                    $actualParameters = array();
-                    foreach ($paremeters as $parameter) {
-                        $actualParameters[] = $args[$parameter->name];
+                if ($reflectionClass->getConstructor()) {
+                    $paremeters = $reflectionClass->getConstructor()->getParameters();
+                    if ($paremeters) {
+                        $actualParameters = array();
+                        foreach ($paremeters as $parameter) {
+                            $actualParameters[] = $args[$parameter->name];
+                        }
+                        $obj = $reflectionClass->newInstanceArgs($actualParameters);
+                        return $obj;
+                    } else {
+                        /*todo*/
                     }
-                    $obj = $reflectionClass->newInstanceArgs($actualParameters);
-                    return $obj;
                 }
             } else {
-                throw new \Exception("class ".$classname."  not found");
+                throw new \Exception("class " . $classname . "  not found");
             }
         } catch (\Exception $e) {
             echo $e->getMessage();
@@ -77,6 +98,9 @@ class Mini
 
     }
 
+    /**
+     * @param $reflectionClass
+     */
     public static function creatObjByReflectionClass($reflectionClass)
     {
 
