3c2e7e46624020cf0275ecb8eaf94811b70dd5ed
lixiao <lixiao.god@163.com>
2018-02-02 11:09:52 +0800

    config

--- app/public/admin.php
+++ app/public/admin.php
@@ -3,8 +3,8 @@ use minicore\lib\MiniApp;
 
 require '../../vendor/autoload.php';
 
-$config=require dirname(dirname(__FILE__)).'/main/config/Config.php';
-$config['params']=require dirname(dirname(__FILE__)).'/main/config/params.php';
+$config=require dirname(dirname(__FILE__)).'/admin/config/Config.php';
+$config['params']=require dirname(dirname(__FILE__)).'/admin/config/params.php';
 (new MiniApp($config))->run();
 
 /*$application = \minicore\lib\Mini::createObj(MiniApp::class,array('config'=>$config));
